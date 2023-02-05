ea7f334a329df77ea788829d634f1bacdd579495
Saba <sabanafchigar@gmail.com>
2023-02-05 15:46:34 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,27 @@
 // switch 
 // just one input
-let x = Number(prompt("x..."))
-switch (x){
-    case 1 :{
-        console.log("shanbe");
-        break;
+let FirstNumber = Number(prompt("Enter First Number"))
+let SecondNumber = Number(prompt("Enter Second Number"))
+let Operand = prompt(" + , - , * , / ")
+switch (Operand){
+    case "+" :{
+        console.log(FirstNumber + SecondNumber);
+        break
     }
-    case 2 :{
-        console.log("1shanbe");
-        break;
+    case "-":{
+        console.log(FirstNumber - SecondNumber);
+        break
     }
-    case 3 :{
-        console.log("2shanbe");
-        break;
+    case "*" :{
+        console.log(FirstNumber * SecondNumber);
+        break
     }
-    case 4 :{
-        console.log("3shanbe");
-        break;
-    }
-    case 5 :{
-        console.log("4shanbe");
-        break;
-    }
-    case 6 :{
-        console.log("5shanbe");
-        break;
-    }
-     case 7 :{
-        console.log("Jome");
-        break;
+    case "/":{
+        console.log(FirstNumber / SecondNumber);
+        break
     }
     default:{
-        console.log("wrong!");
-        break;
+        console.log("Something wrong happened!");
     }
+
 }
